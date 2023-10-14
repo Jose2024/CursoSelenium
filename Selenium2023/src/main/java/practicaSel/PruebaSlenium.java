a396891782e39d1cda2d2a937a8cc0c8d3706e17
Jose E Escobedo C <Jose E Escobedo C@Escon>
2023-10-14 11:48:14 -0600

    ejemplos de selenium

--- Selenium2023/src/main/java/practicaSel/PruebaSlenium.java
+++ Selenium2023/src/main/java/practicaSel/PruebaSlenium.java
@@ -1,16 +1,70 @@
 package practicaSel;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class PruebaSlenium {
 
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.test.com");
+	public static <string> void main(String[] args) throws InterruptedException {
 		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		//obtener objetos /webelemtns de la pagina
 				
+		WebElement userNameTxt = driver.findElement(By.id("user-name"));
+		WebElement passwordTxt = driver.findElement(By.name("password"));
+		WebElement loginBt = driver.findElement(By.xpath("//input[@data-test = 'login-button']"));
 
+	//LOGIN
+		userNameTxt.sendKeys("standard_user");
+		Thread.sleep(2000);
+		passwordTxt.sendKeys("secret_sauce");
+		Thread.sleep(2000);
+		loginBt.click();
+		Thread.sleep(2000);
+
+//Metodos del Explorador
+		String title =  driver.getTitle();
+		System.out.println(" el titulo es: " + title);
+		
+//obtener url
+		String url = driver.getCurrentUrl();
+		System.out.println("La url es: " + url);
+		
+		
+// metodos de navegacion
+		//driver.navigate().back();
+		//Thread.sleep(2000);
+		//driver.navigate().forward();
+		//Thread.sleep(2000);
+		//driver.navigate().refresh();
+		
+// verificar elemento de desplegado en la pagina		
+		boolean prodContainer = driver.findElement(By.className("inventory_item_name")).isDisplayed();
+		if(prodContainer) {
+			System.out.println("El articulo esta desplegado");
+		}
+		
+//obtener text
+		String prodName = driver.findElement(By.className("inventory_item_price")).getText();
+		System.out.println("ell producto es: " + prodName);
+		
+// dropsdown
+		driver.manage().window().maximize();//maximizar ventana
+		Select drpdwnProdSelect = new Select(driver.findElement(By.className("product_sort_container")));
+		drpdwnProdSelect.selectByVisibleText("Price (low to high)");
+		Thread.sleep(3000);
+		drpdwnProdSelect = new Select(driver.findElement(By.className("product_sort_container")));
+	    drpdwnProdSelect.selectByValue("hilo");
+	    
+//cerrar navegador web
+	    //driver.close(); // cierra la ventana que esta usando
+	    driver.quit(); //cierra todas la ventanas que el webdriver usa
+		
+		
 	}
 
 }
